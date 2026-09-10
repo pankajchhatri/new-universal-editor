253cc7b5386c6c682fae138f87302a5d5a848c82
DZCW37A <ext.Pankaj.Chhatri@dmz.skoda-auto.cz>
2026-09-10 19:05:07 +0530

    Styling of quote block

---
+++ blocks/quote/quote.js
@@ -0,0 +1,11 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  const [quoteWrapper] = block.children;
+
+  const blockquote = document.createElement('blockquote');
+  blockquote.textContent = quoteWrapper.textContent.trim();
+  quoteWrapper.replaceChildren(blockquote);
+}
+
